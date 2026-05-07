d3fd4050b24fa279e82891e48a7444edd1c41bcb
Efrén <joseefrensanchezlgmail.com>
2026-05-07 13:26:10 -0600

    Put methods added to students

--- routes/students.py
+++ routes/students.py
@@ -95,3 +95,55 @@ def create_student():
         return {
             "error": str(e)
         }, 500
+
+# =========================
+# UPDATE STUDENT
+# =========================
+
+@students_bp.route("/students/<id>", methods=["PUT"])
+def update_student(id):
+
+    try:
+
+        data = request.json
+
+        conn = get_connection()
+
+        cursor = conn.cursor()
+
+        query = """
+        UPDATE STUDENTS
+        SET
+            NAME_STUDENT = ?,
+            BIRTHDATE = ?,
+            GENDER = ?,
+            ENROLLMENT_YEAR = ?,
+            ID_DEPARTMENT = ?
+        WHERE ID_STUDENT = ?
+        """
+
+        cursor.execute(
+            query,
+            (
+                data.get("name_student"),
+                data.get("birthdate"),
+                data.get("gender"),
+                data.get("enrollment_year"),
+                data.get("id_department"),
+                id
+            )
+        )
+
+        conn.commit()
+
+        conn.close()
+
+        return {
+            "message": "Student updated successfully"
+        }
+
+    except Exception as e:
+
+        return {
+            "error": str(e)
+        }, 500
