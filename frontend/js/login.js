1b8ace1e74545a1f75815f4e5698e005f1bde19b
Efrén <joseefrensanchezlgmail.com>
2026-05-08 23:55:09 -0600

    Enlaze correcto de botones

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,18 +1,15 @@
 async function login(){
 
     const username = document.getElementById("username").value
-
     const password = document.getElementById("password").value
 
     const response = await fetch(
         "http://127.0.0.1:5000/login",
         {
             method: "POST",
-
             headers:{
                 "Content-Type":"application/json"
             },
-
             body: JSON.stringify({
                 username,
                 password
@@ -22,6 +19,8 @@ async function login(){
 
     const data = await response.json()
 
+    console.log("LOGIN RESPONSE:", data)
+
     if(data.success){
 
         localStorage.setItem(
@@ -29,7 +28,7 @@ async function login(){
             JSON.stringify(data.user)
         )
 
-        window.location.href = "dashboard.html"
+        window.location.href = "./dashboard.html"
 
     }else{
 
